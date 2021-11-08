90ca934e19e3b710483e4f80fd7d4507bd0e9ae5
Bartek <supereddek13@gmail.com>
2021-11-08 19:35:23 +0100

    Finished project

--- src/com/company/MainWindow.java
+++ src/com/company/MainWindow.java
@@ -1,33 +1,89 @@
 package com.company;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Map;
 
-public class MainWindow extends JFrame {
+public class MainWindow extends JFrame implements ActionListener {
+
+    JPanel panel;
+    JLabel userLabel, passwordLabel;
+    JTextField userNameInput;
+    JPasswordField userPasswordInput;
+    JButton loginButton, resetButton;
+    static Map<String, String> PASSWORDS = Map.of(
+            "admin", "admin",
+            "user", "haslo",
+            "asia", "123");
 
     public static void main(String[] args) {
 	// write your code here
         MainWindow frame = new MainWindow("Logowanie");
-        frame.setVisible(true);
+        System.out.println(PASSWORDS);
+
     }
 
     public MainWindow(String title) throws HeadlessException {
         super(title);
+        // User Label
+        userLabel = new JLabel();
+        userLabel.setText("User Name :");
+        userNameInput = new JTextField();
 
+        // Password
+        passwordLabel = new JLabel();
+        passwordLabel.setText("Password :");
+        userPasswordInput = new JPasswordField();
+
+        // Login and cancel
+        loginButton = new JButton("Login");
+        resetButton = new JButton("Reset");
+
+        panel = new JPanel(new GridLayout(3, 1));
+        //JPanel panel = new JPanel(null);
+
+        panel.add(userLabel);
+        panel.add(userNameInput);
+        panel.add(passwordLabel);
+        panel.add(userPasswordInput);
+        panel.add(loginButton);
+        panel.add(resetButton);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 600);
-        JPanel contentPanel = new JPanel();
-        contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentPanel);
-        contentPanel.setLayout(null);
-        JButton logBtn = new JButton("Zaloguj");
-        JButton cancBtn = new JButton("Anuluj");
 
-        logBtn.setBounds(300- 90, 400, 90, 23);
-        contentPanel.add(logBtn);
-        cancBtn.setBounds(500, 400, 90, 23);
-        contentPanel.add(cancBtn);
+        // Adding the listeners
+        loginButton.addActionListener(this);
+        resetButton.addActionListener(this);
+        add(panel, BorderLayout.CENTER);
+        setTitle("Logowanie");
+        setSize(400, 180);
+        this.getContentPane().setBackground(Color.BLUE);
+        setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //Login button
+       if(e.getSource() == this.loginButton) {
+           System.out.println("klik");
+           String nameInput = userNameInput.getText();
+           String passwordInput = userPasswordInput.getText();
+
+           if (PASSWORDS.containsKey(nameInput) && PASSWORDS.get(nameInput).equals( passwordInput)){
+               panel.setBackground(Color.GREEN);
+               System.out.println("Działa");
+           } else {
+               panel.setBackground(Color.RED);
+           }
+       }
+
+       if(e.getSource() == this.resetButton) {
+           userNameInput.setText("");
+           userPasswordInput.setText("");
+           panel.setBackground(Color.WHITE);
+       }
 
 
     }
